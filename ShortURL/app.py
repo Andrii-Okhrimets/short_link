ab6cbe4b67cd5da19a368687ea1bb4d4051efa60
mebel-akvareli <37705950+mebel-akvareli@users.noreply.github.com>
2021-09-08 17:02:44 +0300

    Update app.py

--- ShortURL/app.py
+++ ShortURL/app.py
@@ -1,55 +1,37 @@
-# Подключаем основные библиотеки
 from flask import Flask, g, render_template, session, redirect
 import sqlite3
-
-# Подключаем библотеку flask-wtf
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-# Подключаем библиотеки необходимые для генерации ссылки
 import random
 import string
 import datetime
 
-# Подключим библиотеку для проверки
 import re, urllib
 
-# Создаём приложение
 app = Flask(__name__)
 
-# Добавляем в файл конфигурации SECRET_KEY, необходимо для нормальной работы Flask-WTF
-app.config['SECRET_KEY'] = "Don't Panic! 42!"
+app.config['SECRET_KEY'] = "Hello<I<am>Andrii!!!"
 app.permanent_session_lifetime = datetime.timedelta(hours=1)
 
-# Название файла БД
 DATABASE = 'ShortURL.db'
 
-
 class LinkForm(FlaskForm):
-    '''
-    Класс - форма для ввода длинной ссылки
-    '''
     name = StringField("Введите длинную ссылку", validators=[DataRequired()])
     submit = SubmitField('ОК')
 
-
-# Следующие две функции необходимы для работы с БД
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
-
-@app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
-
-# Функция - запрос в БД. В случае необходимости, может закомитить информацию
 def query_db(query, args=(), one=False, commit=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
@@ -58,9 +40,6 @@ def query_db(query, args=(), one=False, commit=False):
         get_db().commit()
     return (rv[0] if rv else None) if one else rv
 
-
-# Создаёт короткую ссылку, пытается вставить в БД, при возникновении ошибки,
-# вызванной повторным ключом, перезапускается
 def generate_short_link(long_link):
     while True:
         try:
@@ -71,13 +50,7 @@ def generate_short_link(long_link):
             return short_link
         except:
             pass
-
-
-# Заглавная страница
-# Здесь пользователь вводит URL, который сразу дополняется,
-# при необходимости, до полный и проходит проверку с помощью
-# регулярного выражения. Затем добавляется информация в БД и пользователь
-# получает страницу с сокращённой ссылкой
+        
 @app.route('/', methods=["GET", "POST"])
 def que():
     session.permanent = True
@@ -94,9 +67,6 @@ def que():
             return render_template('index3.html')
     return render_template('index.html', form=link_input)
 
-
-# При переходе по адресу, эта функция проверяет, есть ли такой короткий адрес в БД.
-# Если есть - выполняется переход по ссылке.
 @app.route('/<short_link>')
 def short_link_redirect(short_link):
     try:
